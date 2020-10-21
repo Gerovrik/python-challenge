84e64f4392ecd11d4057ece8d3fdb36b67b38fd3
Gerorik <loganlippert@yahoo.com>
2020-10-21 18:09:27 -0500

    First push of Files

---
+++ PyBank/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#Main script for PyBank
+"""
+Created on Wed Oct 21 18:05:51 2020
+
+@author: Logan
+ 
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#Main script for PyPoll
+"""
+Created on Wed Oct 21 18:07:14 2020
+
+@author: Logan
+"""
+
