e28acc39106b0711e20eca4305ba268b34ff5725
Gerorik <loganlippert@yahoo.com>
2020-10-24 09:50:26 -0500

    Final Copy

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,73 @@
 # -*- coding: utf-8 -*-
-#Main script for PyBank
+#Main script for PyBank written in spyder (python 3.8)
 """
 Created on Wed Oct 21 18:05:51 2020
 
 @author: Logan
  
 """
+#*****************************************************************************
+#importing Dependencies
+import csv
+#*****************************************************************************
+#Declare the file
+raw_Data="C:\\Users\\Logan\\Desktop\\bootcamp\\Python-challenge\\python-challenge\\PyBank\\Resources\\budget_data.csv"
 
+#*****************************************************************************
+#Empty lists declared
+t_Profit = []
+months = []
+avg_Change = []  
+ 
+#*****************************************************************************       
+#Opening/read csv file
+with open(raw_Data, newline='', encoding="utf-8") as bd_File:
+        csvreader = csv.reader(bd_File, delimiter= ",")
+        csvheader = next(bd_File)
+        
+#*****************************************************************************
+#reading in each row of data skipping header               
+        for r in csvreader:
+            months.append(r[0])
+            t_Profit.append(int(r[1]))
+            
+#*****************************************************************************
+# finding average change 
+        for x in range(1, len(t_Profit)):
+            avg_Change.append((int(t_Profit[x]) - int(t_Profit[x-1])))
+
+#*****************************************************************************
+# Calculations            
+        t_Avg = sum(avg_Change)/len(avg_Change)
+        t_Months =  len(months)
+        max_Increase = max(avg_Change)
+        max_Month = months[avg_Change.index(max(avg_Change))+1]
+        max_Decrease = min(avg_Change)
+        min_Month = months[avg_Change.index(min(avg_Change))+1]
+        
+        
+#*****************************************************************************
+#Print statements for each section        
+        print(f'Finacial Analysis')
+        print(f'------------------------------------------')
+        print(f'Total Months: \t    {t_Months}')
+        print(f'Total: \t\t\t $ {sum(avg_Change)}')
+        print(f'Average Change:  $ {round(float(t_Avg),2)}')
+        print(f'Greatest Increase in Profit: {max_Month} $ {max_Increase}')
+        print(f'Greatest Decrease in Profit: {min_Month} $ {max_Decrease} ')
+#*****************************************************************************        
+#Printing to file 
+output_File="C:\\Users\\Logan\\Desktop\\bootcamp\\Python-challenge\\python-challenge\\PyBank\\Resources\\Financial_Analysis.csv"
+
+with open(output_File,"w") as file:
+    
+    file.write(f'Finacial Analysis \n')
+    file.write(f'------------------------------------------\n')
+    file.write(f'Total Months: {t_Months} \n')
+    file.write(f'Total: $ {sum(avg_Change)} \n')
+    file.write(f'Average Change:  $ {round(float(t_Avg),2)} \n')
+    file.write(f'Greatest Increase in Profit: {max_Month} $ {max_Increase} \n')
+    file.write(f'Greatest Decrease in Profit: {min_Month} $ {max_Decrease} \n')
+    
+
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,4 +5,91 @@ Created on Wed Oct 21 18:07:14 2020
 
 @author: Logan
 """
+#*****************************************************************************
+#importing Dependencie
+import csv
+
+#*****************************************************************************
+#Declare the file
+raw_Data="C:\\Users\\Logan\\Desktop\\bootcamp\\Python-challenge\\python-challenge\\PyPoll\\Resources\\election_data.csv"
+
+#*****************************************************************************
+#Variables initialized
+t_Votes = 0; Khan_votes = 0; Correy_votes = 0; Li_votes = 0; Otooley_votes = 0
+
+#*****************************************************************************
+#Opening/read csv file
+
+with open(raw_Data, newline="", encoding = "utf-8") as Votes:
+    
+    csv_Reader = csv.reader(Votes,delimiter = ",")
+    
+    header = next(csv_Reader)
+#*****************************************************************************
+#Setting up For loop to put csv into lists
+    for i in csv_Reader:
+        
+        voter_Id =   i[0]
+        county =     i[1]
+        candidate =  i[2]
+        
+        # Total votes iteration and addition
+        t_Votes +=1 
+        
+        #ifelse statement for counting votes of each candidate
+        if candidate == "Khan":
+            Khan_votes +=1 
+        elif candidate == "Correy":
+            Correy_votes +=1 
+        elif candidate == "Li":
+            Li_votes +=1 
+        elif candidate == "O'Tooley":
+            Otooley_votes +=1 
+            
+#*****************************************************************************
+#Creating a dictionary then finding Max votes to get winner
+Who = ["Khan", "Correy", "Li", "O'Tooley"]
+p_V = [Khan_votes, Correy_votes, Li_votes, Otooley_votes]
+candidate_Outcome=  dict(zip(Who, p_V))
+winner = max(candidate_Outcome,  key=candidate_Outcome.get)
+
+#*****************************************************************************
+#Doing math for Percent of total votes for each candidate
+Kahn_p =        (Khan_votes / t_Votes)*100
+Correy_p =      (Correy_votes / t_Votes)*100
+Li_p =          (Li_votes / t_Votes)*100
+Otooley_p =     (Otooley_votes / t_Votes)*100
+
+#*****************************************************************************
+#print out statements for each results
+print(f'Election Results')
+print(f'-------------------------')
+print(f'Total Votes: {t_Votes}')
+print(f'-------------------------')
+print(f'Kahn: {Kahn_p:.3}% {Khan_votes}')
+print(f'Correy: {Correy_p:.3}% {Correy_votes}')
+print(f'Li: {Li_p:.3}% {Li_votes}')
+print(f'OTooley: {Otooley_p:.3}% {Otooley_votes}')
+print(f'-------------------------')
+print(f'Winner: {winner}')
+print(f'-------------------------')
+#*****************************************************************************
+#creating output file using With open to write to file
+output_File="C:\\Users\\Logan\\Desktop\\bootcamp\\Python-challenge\\python-challenge\\PyBank\\Resources\\Poll_Results.csv"
+
+with open(output_File,"w") as file:
+    
+    file.write(f'Election Results')
+    file.write(f'-------------------------')
+    file.write(f'Total Votes: {t_Votes}')
+    file.write(f'-------------------------')
+    file.write(f'Kahn: {Kahn_p:.3}% {Khan_votes}')
+    file.write(f'Correy: {Correy_p:.3}% {Correy_votes}')
+    file.write(f'Li: {Li_p:.3}% {Li_votes}')
+    file.write(f'OTooley: {Otooley_p:.3}% {Otooley_votes}')
+    file.write(f'-------------------------')
+    file.write(f'Winner: {winner}')
+    file.write(f'-------------------------')
+     
+
 
